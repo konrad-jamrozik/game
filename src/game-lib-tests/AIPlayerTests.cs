34623c3c5b6f95a3f1f5a287a727cd70682a48c9
Konrad Jamrozik <kojamroz@microsoft.com>
2023-07-07 22:57:39 -0700

    add support for multiple AIPlayer intellects

--- src/game-lib-tests/AIPlayerTests.cs
+++ src/game-lib-tests/AIPlayerTests.cs
@@ -10,9 +10,24 @@ public class AIPlayerTests
     }
 
     [Test]
-    public void AIPlayerPlaysGameUntilConclusion()
+    public void AIPlayerWithDoNothingIntellectPlaysGameUntilConclusion()
+        => AIPlayerPlaysGameUntilConclusion(AIPlayer.Intellect.DoNothing);
+
+    [Test]
+    public void AIPlayerWithOnlySendAgentsOnMissionsIntellectPlaysGameUntilConclusion()
+        => AIPlayerPlaysGameUntilConclusion(AIPlayer.Intellect.OnlySendAgentsOnMissions);
+
+    [Test]
+    public void AIPlayerWithBasicIntellectPlaysGameUntilConclusion()
+        => AIPlayerPlaysGameUntilConclusion(AIPlayer.Intellect.Basic);
+
+    public void AIPlayerPlaysGameUntilConclusion(AIPlayer.Intellect intellect)
     {
-        var aiPlayer = new AIPlayer(new GameSessionController(new GameSession()));
+        var aiPlayer = new AIPlayer(new GameSessionController(new GameSession()), intellect);
+
+        // Act
         aiPlayer.PlayGameSession();
+
+        // Assert: no exception was thrown and the program didn't loop indefinitely.
     }
 }
--- src/game-lib/AIPlayer.cs
+++ src/game-lib/AIPlayer.cs
@@ -8,7 +8,7 @@ namespace UfoGameLib;
 ///
 /// - Advance time until mission is available
 /// - Once mission available, hire agents up to transport limit and send on the first available mission
-/// - Repeat until player loses game. At this point it impossible to win. Loss will most likely happend
+/// - Repeat until player loses game. At this point it impossible to win. Loss will most likely happen
 /// due to running out of money.
 ///
 /// kja2 improvements to AIPlayer:
@@ -19,16 +19,47 @@ namespace UfoGameLib;
 /// </summary>
 public class AIPlayer : AbstractAIPlayer
 {
+    private readonly Intellect _intellect;
+
+    public enum Intellect
+    {
+        DoNothing,
+        OnlySendAgentsOnMissions,
+        Basic
+    }
+
+    private readonly IDictionary<Intellect, Action<GameStatePlayerView, GameSessionController>> _intellectMap =
+        new Dictionary<Intellect, Action<GameStatePlayerView, GameSessionController>>
+        {
+            [Intellect.DoNothing] = PlayGameTurnWithDoNothingIntellect,
+            [Intellect.OnlySendAgentsOnMissions] = PlayGameTurnWithOnlySendAgentsOnMissionsIntellect,
+            [Intellect.Basic] = PlayGameTurnWithBasicIntellect
+        };
+
     private const int MaxAgentsToSendOnMission = 2;
 
-    public AIPlayer(GameSessionController controller) : base(controller)
+    public AIPlayer(GameSessionController controller, Intellect intellect) : base(controller)
     {
+        _intellect = intellect;
     }
 
     protected override void PlayGameTurn(GameSessionController controller)
     {
         GameStatePlayerView gameStateView = controller.GameStatePlayerView;
-            
+        _intellectMap[_intellect](gameStateView, controller);
+    }
+
+    private static void PlayGameTurnWithDoNothingIntellect(
+        GameStatePlayerView gameStateView,
+        GameSessionController controller)
+    {
+        // Do nothing.
+    }
+
+    private static void PlayGameTurnWithOnlySendAgentsOnMissionsIntellect(
+        GameStatePlayerView gameStateView,
+        GameSessionController controller)
+    {
         while (CanLaunchSomeMission(gameStateView))
         {
             MissionSite targetSite = ChooseMissionSite(gameStateView);
@@ -37,13 +68,31 @@ public class AIPlayer : AbstractAIPlayer
         }
     }
 
+    private static void PlayGameTurnWithBasicIntellect(
+        GameStatePlayerView gameStateView,
+        GameSessionController controller)
+    {
+        // kja current work
+        // Agents should be always occupied. Doing one of the following:
+        // - being sent on a mission
+        // - recovering from wounds
+        // - training to improve
+        // - gathering intelligence
+        // - generating income
+        //
+        // The AIPlayer also needs to take into account the following:
+        // - Are there any missions? Are they easy enough to send agents to?
+        // - Is there enough money in the bank? Income/Expenses are OK?
+        // - Are there enough agents available, or more need to be hired?
+    }
+
     private static bool CanLaunchSomeMission(GameStatePlayerView gameStateView)
         => gameStateView.MissionSites.Any(site => site.IsActive) && gameStateView.Assets.CurrentTransportCapacity > 0;
 
     private static MissionSite ChooseMissionSite(GameStatePlayerView gameStateView)
         => gameStateView.MissionSites.First(site => site.IsActive);
 
-    private void HireAgentsIfNecessary(GameStatePlayerView gameStateView, GameSessionController controller)
+    private static void HireAgentsIfNecessary(GameStatePlayerView gameStateView, GameSessionController controller)
     {
         // If there are less than MaxAgentsToSendOnMission then hire enough agents to reach MaxAgentsToSendOnMission,
         // however, no more than remaining transport capacity, i.e. CurrentTransportCapacity.
