2fe596c1212e84658490f82c16f205f28510cc6a
Konrad Jamrozik <kojamroz@microsoft.com>
2023-07-14 23:14:28 -0700

    refactor LogPrefix method to "switch expression" syntax

--- src/game-lib/Infra/Log.cs
+++ src/game-lib/Infra/Log.cs
@@ -16,47 +16,40 @@ public class Log : ILog
         Console.WriteLine($"Created Log with log file path {_config.LogFile.FullPath}");
     }
 
+    public void Info(
+        string message,
+        [CallerFilePath] string? callerFilePath = null,
+        [CallerMemberName] string? callerMemberName = null)
+    {
+        string log = LogPrefix(callerFilePath, callerMemberName) + message;
+        Console.WriteLine(log);
+        _logs.AppendLine(log);
+    }
+
     public void Dispose()
     {
         _config.LogFile.WriteAllText(_logs.ToString());
         Console.WriteLine($"Wrote logs to {_config.LogFile.FullPath}");
     }
 
-    public void Info(
-        string message,
-        [CallerFilePath] string? callerFilePath = null,
-        [CallerMemberName] string? callerMemberName = null)
-
-    {
-        // Based on https://stackoverflow.com/a/45512962/986533
-
-        string log = LogPrefix(callerFilePath, callerMemberName) + message;
-        Console.WriteLine(log);
-        _logs.AppendLine(log);
-    }
-
     private string LogPrefix(string? callerFilePath, string? callerMemberName)
     {
-        string logPrefix = "";
-        switch (_config)
+        return _config switch
         {
-            case { IncludeCallerTypeNameInLog: true, IncludeCallerMemberNameInLog: true }:
-            {
-                var callerTypeName = Path.GetFileNameWithoutExtension(callerFilePath);
-                logPrefix = $"{callerTypeName}.{callerMemberName}: ";
-                break;
-            }
-            case { IncludeCallerTypeNameInLog: true, IncludeCallerMemberNameInLog: false }:
-            {
-                var callerTypeName = Path.GetFileNameWithoutExtension(callerFilePath);
-                logPrefix = $"{callerTypeName}: ";
-                break;
-            }
-            case { IncludeCallerTypeNameInLog: false, IncludeCallerMemberNameInLog: true }:
-                logPrefix = $"{callerMemberName}: ";
-                break;
-        }
+            { IncludeCallerTypeNameInLog: true, IncludeCallerMemberNameInLog: true }
+                => $"{CallerTypeName()}.{callerMemberName}: ",
 
-        return logPrefix;
+            { IncludeCallerTypeNameInLog: true, IncludeCallerMemberNameInLog: false }
+                => $"{CallerTypeName()}: ",
+
+            { IncludeCallerTypeNameInLog: false, IncludeCallerMemberNameInLog: true }
+                => $"{callerMemberName}: ",
+
+            _ => ""
+        };
+
+        // Based on https://stackoverflow.com/a/45512962/986533
+        string? CallerTypeName()
+            => Path.GetFileNameWithoutExtension(callerFilePath);
     }
 }
