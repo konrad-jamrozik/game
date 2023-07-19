0fd385ee6e7f112975c3707fc45c196daab0d592
Konrad Jamrozik <kojamroz@microsoft.com>
2023-07-18 23:42:08 -0700

    update todos

--- src/game-lib/Players/BasicAIPlayerIntellect.cs
+++ src/game-lib/Players/BasicAIPlayerIntellect.cs
@@ -19,8 +19,6 @@ public class BasicAIPlayerIntellect : IAIPlayerIntellect
 
     public void PlayGameTurn(GameStatePlayerView state, GameSessionController controller)
     {
-        // kja2 curr work PlayGameTurnWithBasicIntellect: see also note at file bottom.
-
         int agentsToHire = ComputeAgentsToHire(state);
         if (agentsToHire > 0)
             controller.HireAgents(agentsToHire);
@@ -255,32 +253,3 @@ public class BasicAIPlayerIntellect : IAIPlayerIntellect
         Debug.Assert(state.Assets.Agents.Available.Count == 0);
     }
 }
-
-// Need to add failure criteria that is not just a hard time limit:
-// Add "Red Dawn remnants" faction that gains power over time
-// and generates harder missions.
-// Mission failures mean support decrease.
-// Once support reaches zero, game is over.
-//
-// So first need to add the concept of mission difficulty.
-//
-// -----
-//
-// Agents should be always occupied. Doing one of the following:
-// - being sent on a mission
-// - recovering from wounds
-// - training to improve
-// - gathering intelligence
-// - generating income
-//
-// The AIPlayer also needs to take into account the following:
-// - Are there any missions? Are they easy enough to send agents to?
-// - Is there enough money in the bank? Income/Expenses are OK?
-// - Are there enough agents available, or more need to be hired?
-//
-// -----
-//
-// - Try to always keep at least enough agents to maintain full transport capacity
-// - Send agents on intel-gathering duty until mission is available
-// - Send agents on available mission if not too hard
-// - Do not hire agents if it would lead to bankruptcy
