e1008bd42cb40fce13eccf10da2cd2e08bda052d
Konrad Jamrozik <kojamroz@microsoft.com>
2023-07-29 16:43:15 -0700

    Add OpenAPI support to the minimal API
    Following:
    https://learn.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis/openapi?view=aspnetcore-8.0

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,10 +1,35 @@
+using Lib.Json;
 using UfoGameLib.Lib;
+using UfoGameLib.State;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-var randomGen = new RandomGen(new Random());
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.MapGet("/", () => $"Hello World! Coin flip: {randomGen.FlipCoin()}");
+app.MapGet(
+        "/helloCoinFlip",
+        () =>
+        {
+            var randomGen = new RandomGen(new Random());
+            return $"Hello World! Coin flip: {randomGen.FlipCoin()}";
+        })
+    .WithName("HelloCoinFlip")
+    .WithOpenApi();
+
+app.MapGet("/initialGameState", () =>
+{
+    var gameSession = new GameSession(new RandomGen(new Random()));
+    string gameStateJson = gameSession.CurrentGameState.ToIndentedUnsafeJsonString(GameSession.StateJsonSerializerOptions);
+    return gameStateJson;
+})
+    .WithName("InitialGameState")
+    .WithOpenApi();
+
 
 app.Run();
