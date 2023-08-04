7ab4b292817bb071c4ff943c1168fe65c4c5bdf3
Konrad Jamrozik <kojamroz@microsoft.com>
2023-08-03 22:45:15 -0700

    added Tailwind Elements per article from Tailwind Elements / Integrations / Solid
    The article:
    https://tailwind-elements.com/docs/standard/integrations/solid-integration/

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,10 +2,12 @@
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [require("tw-elements/dist/plugin.cjs")]
 }
 
