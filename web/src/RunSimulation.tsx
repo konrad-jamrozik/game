6d6dd6e46e274c42342bff4feb86376e8e6df1f3
Konrad Jamrozik <kojamroz@microsoft.com>
2023-08-04 02:50:57 -0700

    frontend: add tailwind-elements-based button that when clicked, gets data from backend and displays it in a paragraph.

---
+++ web/src/RunSimulation.tsx
@@ -0,0 +1,48 @@
+import { onCleanup } from 'solid-js';
+import { createSignal } from "solid-js";
+
+function RunSimulation() {
+
+  const [message, setMessage] = createSignal("");
+  setMessage("pending");
+
+  const handleClick = () => {
+    
+    const apiUrl = 'https://api-game-lib.azurewebsites.net/exampleGameSession';
+
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        setMessage(`Done! Money: ${data.Assets.Money}`);
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error('Error fetching data:', error);
+    });
+
+    onCleanup(() => {
+    });
+
+  };
+
+  return (
+    <div>
+      <Button onClick={() => handleClick()} />
+      <p class="mb-4 text-center">Simulation result: {message()}</p>
+      
+    </div>
+  );
+}
+
+function Button(props: { onClick: () => void }) {
+  return (
+    <button
+      class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+      onClick={props.onClick}
+    >
+      Run simulation for X turns
+    </button>
+  )
+}
+
+export default RunSimulation;
