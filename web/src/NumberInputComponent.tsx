26cb8e8bd3695db6fd262bacece26065592c7b0d
Konrad Jamrozik <kojamroz@microsoft.com>
2023-08-06 00:26:51 -0700

    add input bounds

--- web/src/NumberInputComponent.tsx
+++ web/src/NumberInputComponent.tsx
@@ -12,6 +12,8 @@ function NumberInputComponent(props: {
     <div class="relative mb-3" data-te-input-wrapper-init>
       <input
         type="number"
+        min="1"
+        max="300"
         class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
         id="numberInputId"
         value={props.input()}
