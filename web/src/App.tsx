ef2170f20155ad5e982556b2c352a6d2434dca8f
Konrad Jamrozik <kojamroz@microsoft.com>
2024-01-07 20:07:44 -0800

    fix lint: remove unused eslint directive

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '../public/vite.svg'
 import './App.css'
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
 function App(): ReactElement {
   const [count, setCount] = useState(0)
 
