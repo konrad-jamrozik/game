a05b9c3fe7762a6d2c02e90aec142f46d1503fa2
Konrad Jamrozik <kojamroz@microsoft.com>
2024-01-19 21:25:17 -0800

    set default turnLimit to 1

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,7 +7,7 @@ export default function App(): React.JSX.Element {
   const [agents, setAgents] = useState<Agent[]>([
     { Id: 42, CurrentState: 'Happy', TurnHired: 66 },
   ])
-  const [turnLimit, setTurnLimit] = useState<number>(0)
+  const [turnLimit, setTurnLimit] = useState<number>(1)
   return (
     <>
       <RunSimulation {...{ agents, setAgents, turnLimit, setTurnLimit }} />
