97a813b6cc18a9c50d88ffb3719fc40f9d8b96a1
Konrad Jamrozik <kojamroz@microsoft.com>
2024-01-20 20:28:00 -0800

    Add axis limits and labels to PrototypeChart

--- web/src/components/PrototypeChart.tsx
+++ web/src/components/PrototypeChart.tsx
@@ -3,6 +3,9 @@ import _ from 'lodash'
 
 import type { GameStatePlayerView } from '../types/GameStatePlayerView'
 
+const defaultXAxisMax = 10
+const defaultYAxisMax = 100
+
 export type PrototypeChartProps = {
   readonly gameStates: readonly GameStatePlayerView[]
 }
@@ -10,9 +13,29 @@ export type PrototypeChartProps = {
 export function PrototypeChart(props: PrototypeChartProps): React.JSX.Element {
   const turns: number[] = _.map(props.gameStates, (gs) => gs.CurrentTurn)
   const money: number[] = _.map(props.gameStates, (gs) => gs.Assets.Money)
+  const xAxisMax = _.max(turns) ?? defaultXAxisMax
+  const yAxisMax = _.max(money) ?? defaultYAxisMax
+
   return (
     <LineChart
-      xAxis={[{ data: turns }]}
+      xAxis={[
+        {
+          data: turns,
+          min: 1,
+          max: xAxisMax,
+          label: 'Turn',
+          scaleType: 'linear',
+        },
+      ]}
+      yAxis={[
+        {
+          min: 0,
+          max: yAxisMax,
+          label: 'Money',
+          scaleType: 'linear',
+          //labelStyle: { ??? },
+        },
+      ]}
       series={[
         {
           data: money,
