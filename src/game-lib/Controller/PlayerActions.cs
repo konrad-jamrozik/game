b6c3d016f4e2eacf7cbb4da22e16e712875199de
Konrad Jamrozik <kojamroz@microsoft.com>
2024-02-02 21:16:03 -0800

    fix bug with too strict assertion

--- src/game-lib/Controller/PlayerActions.cs
+++ src/game-lib/Controller/PlayerActions.cs
@@ -23,8 +23,15 @@ public class PlayerActions : List<PlayerAction>
             // for example, 10 agents can be hired by invoking "Hire agents" action 10 times with 1 agent,
             // 5 times with 2 agents or 1 time with 10 agents.
             state.UpdateCount += 1;
+            if (action is not AdvanceTimePlayerAction)
+            {
+                Debug.Assert(!state.IsGameOver, 
+                    $"money: {state.Assets.Money} " +
+                    $"funding: {state.Assets.Funding} " +
+                    $"support: {state.Assets.Support}");
+            }
         });
-        Debug.Assert(!state.IsGameOver);
+
         // Currently GameStateUpdateLog is just a stub.
         return new GameStateUpdateLog();
     }
