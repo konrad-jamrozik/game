d1809911212a802154ebc9e63a55872090d1ab4c
Konrad Jamrozik <kojamroz@microsoft.com>
2024-02-20 00:47:32 -0800

    refactor ruleset.ts

--- web/src/lib/codesync/ruleset.ts
+++ web/src/lib/codesync/ruleset.ts
@@ -1,17 +1,30 @@
 // codesync UfoGameLib.Model.Ruleset
-// kja3: instead of replicating this logic from backend here,
-// make backend expose this property in the returned json. Applies to all such
-// properties in ruleset.ts.
+// Note: ideally I would auto-generate the ruleset.ts file from the backend code,
+// but there doesn't appear to be a well-supported, official solution for this.
 import _ from 'lodash'
 import type { Agent, AgentState, Mission, MissionSite } from './GameState'
 import type { AgentPlayerActionName } from './PlayerActionPayload'
 
 export const agentUpkeepCost = 5
+
 export const incomeGeneratedPerAgent = agentUpkeepCost * 3
 
 export const agentHireCost = 50
 
-export const agentTrainingCoefficient = 1
+const agentTrainingCoefficient = 1
+
+const transportCapBuyCost = 200
+
+const skillFromEachFirstMission = [18, 15, 12, 9, 6]
+
+const skillFromEachMissionBeyondFirstMissions =
+  skillFromEachFirstMission.at(-1)!
+
+const minSurvivalThreshold = 1
+
+const agentSurvivalRollUpperBound = 100
+
+const baseMissionSiteDifficulty = 30
 
 export function isActive(missionSite: MissionSite): boolean {
   return (
@@ -33,6 +46,65 @@ export function getSurvivalSkill(agent: Agent): number {
   )
 }
 
+export function transportCapBuyingCost(cap: number): number {
+  return cap * transportCapBuyCost
+}
+
+export function canBeSentOnMission(agent: Agent): boolean {
+  const validAgentStates: AgentState[] = ['Available', 'Training']
+  return _.includes(validAgentStates, agent.CurrentState)
+}
+
+export function getSurvivalChanceNonNegative(
+  agent: Agent,
+  missionSite: MissionSite,
+): number {
+  return _.max([getSurvivalChance(agent, missionSite), 0])!
+}
+
+export function requiredSurvivingAgentsForSuccess(site: MissionSite): number {
+  const reqAgentsForSuccess =
+    1 + Math.floor((site.Difficulty - baseMissionSiteDifficulty) / 30)
+  console.assert(reqAgentsForSuccess >= 1)
+  return reqAgentsForSuccess
+}
+
+export const agentPlayerActionConditionMap: {
+  [action in AgentPlayerActionName]: (agent: Agent) => boolean
+} = {
+  SendAgentsToIncomeGeneration: canBeSentOnMission,
+  SendAgentsToIntelGathering: canBeSentOnMission,
+  SendAgentsToTraining: canBeSentToTraining,
+  RecallAgents: canBeRecalled,
+  SackAgents: canBeSacked,
+}
+
+function canBeRecalled(agent: Agent): boolean {
+  const validStates: AgentState[] = ['GatheringIntel', 'GeneratingIncome']
+  return _.includes(validStates, agent.CurrentState)
+}
+
+function canBeSacked(agent: Agent): boolean {
+  const validStates: AgentState[] = ['Available', 'Training']
+  return _.includes(validStates, agent.CurrentState)
+}
+
+function canBeSentToTraining(agent: Agent): boolean {
+  const validStates: AgentState[] = ['Available']
+  return _.includes(validStates, agent.CurrentState)
+}
+
+function getSurvivalChance(agent: Agent, missionSite: MissionSite): number {
+  return (
+    agentSurvivalRollUpperBound -
+    agentSurvivalThreshold(agent, missionSite.Difficulty)
+  )
+}
+
+function agentSurvivalThreshold(agent: Agent, difficulty: number): number {
+  return _.max([difficulty - getSurvivalSkill(agent), minSurvivalThreshold])!
+}
+
 function skillFromMissions(agent: Agent): number {
   const skillFromFirstMissions = _.reduce(
     skillFromEachFirstMission.slice(0, agent.MissionsSurvived),
@@ -47,83 +119,4 @@ function skillFromMissions(agent: Agent): number {
     skillFromFirstMissions +
     missionsBeyondFirstMissions * skillFromEachMissionBeyondFirstMissions
   )
-}
-
-const skillFromEachFirstMission = [18, 15, 12, 9, 6]
-
-const skillFromEachMissionBeyondFirstMissions =
-  skillFromEachFirstMission.at(-1)!
-
-export function transportCapBuyingCost(cap: number): number {
-  return cap * 200
-}
-
-export function canBeSentOnMission(agent: Agent): boolean {
-  const validAgentStates: AgentState[] = ['Available', 'Training']
-  return _.includes(validAgentStates, agent.CurrentState)
-}
-
-export const minSurvivalThreshold = 1
-export const agentSurvivalRollUpperBound = 100
-
-export function agentSurvivalThreshold(
-  agent: Agent,
-  difficulty: number,
-): number {
-  return _.max([difficulty - getSurvivalSkill(agent), minSurvivalThreshold])!
-}
-
-export function getSurvivalChanceNonNegative(
-  agent: Agent,
-  missionSite: MissionSite,
-): number {
-  return _.max([getSurvivalChance(agent, missionSite), 0])!
-}
-
-export function getSurvivalChance(
-  agent: Agent,
-  missionSite: MissionSite,
-): number {
-  return (
-    agentSurvivalRollUpperBound -
-    agentSurvivalThreshold(agent, missionSite.Difficulty)
-  )
-}
-
-export function canSurvive(agent: Agent, missionSite: MissionSite): boolean {
-  return getSurvivalChance(agent, missionSite) > 0
-}
-
-export const baseMissionSiteDifficulty = 30
-
-export function requiredSurvivingAgentsForSuccess(site: MissionSite): number {
-  const reqAgentsForSuccess =
-    1 + Math.floor((site.Difficulty - baseMissionSiteDifficulty) / 30)
-  console.assert(reqAgentsForSuccess >= 1)
-  return reqAgentsForSuccess
-}
-
-export function canBeRecalled(agent: Agent): boolean {
-  const validStates: AgentState[] = ['GatheringIntel', 'GeneratingIncome']
-  return _.includes(validStates, agent.CurrentState)
-}
-
-export function canBeSacked(agent: Agent): boolean {
-  const validStates: AgentState[] = ['Available', 'Training']
-  return _.includes(validStates, agent.CurrentState)
-}
-
-export function canBeSentToTraining(agent: Agent): boolean {
-  const validStates: AgentState[] = ['Available']
-  return _.includes(validStates, agent.CurrentState)
-}
-
-export const agentPlayerActionConditionMap: {
-  [action in AgentPlayerActionName]: (agent: Agent) => boolean
-} = {
-  SendAgentsToIncomeGeneration: canBeSentOnMission,
-  SendAgentsToIntelGathering: canBeSentOnMission,
-  SendAgentsToTraining: canBeSentToTraining,
-  RecallAgents: canBeRecalled,
-  SackAgents: canBeSacked,
 }
