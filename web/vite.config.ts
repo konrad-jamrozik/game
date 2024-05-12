48863d23ab0e430b7e6655e42a2af740948e17b3
Konrad Jamrozik <kojamroz@microsoft.com>
2024-05-11 22:18:18 -0700

    remove unnecessary "@vitejs/plugin-basic-ssl"

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,21 +1,10 @@
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [
-    react(),
-    basicSsl({
-      /** name of certification */
-      name: 'test',
-      /** custom trust domains */
-      domains: ['*.custom.com'],
-      /** custom certification directory */
-      certDir: '/Users/.../.devServer/cert',
-    }),
-  ],
+  plugins: [react()],
   optimizeDeps: {
     // This:
     //   include: ['@mui/material/Tooltip', '@emotion/styled', '@mui/material/Unstable_Grid2'],
