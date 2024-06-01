0a7c13af60f55abf880637f7c750d1d70035ccc0
Konrad Jamrozik <kojamroz@microsoft.com>
2024-06-01 15:55:45 -0700

    refactor to actionToDisplayedDetailsMap

--- web/src/lib/rendering/renderPlayerActionPayload.ts
+++ web/src/lib/rendering/renderPlayerActionPayload.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/consistent-return */
 /* eslint-disable default-case */
+import _ from 'lodash'
 import type {
   PlayerActionName,
   PlayerActionPayload,
@@ -18,42 +19,48 @@ const actionToDisplayedTypeMap: { [name in PlayerActionName]: string } = {
   RecallAgents: 'Recall agents',
 }
 
+const actionToDisplayedDetailsMap: { [name in PlayerActionName]: string } = {
+  AdvanceTime: '',
+  BuyTransportCap: '',
+  HireAgents: `Count: $TargetID`,
+  LaunchMission: `Agent IDs: $IDs, Site: $TargetID`,
+  SackAgents: `Agent IDs: $IDs`,
+  SendAgentsToIncomeGeneration: `Agent IDs: $IDs`,
+  SendAgentsToIntelGathering: `Agent IDs: $IDs`,
+  SendAgentsToTraining: `Agent IDs: $IDs`,
+  RecallAgents: `Agent IDs: $IDs`,
+}
+
 export function getDisplayedType(action: PlayerActionPayload): string {
   return actionToDisplayedTypeMap[action.Action]
 }
 
 export function getDisplayedDetails(action: PlayerActionPayload): string {
-  switch (action.Action) {
-    case 'AdvanceTime': {
-      return ''
-    }
-    case 'BuyTransportCap': {
-      return ''
-    }
-    case 'HireAgents': {
-      return `Count: ${action.TargetId}`
-    }
-    case 'LaunchMission': {
-      return `Agent IDs: ${logIds(action)}, Site: ${action.TargetId}`
-    }
-    case 'SackAgents': {
-      return `Agent IDs: ${logIds(action)}`
-    }
-    case 'SendAgentsToIncomeGeneration': {
-      return `Agent IDs: ${logIds(action)}`
-    }
-    case 'SendAgentsToIntelGathering': {
-      return `Agent IDs: ${logIds(action)}`
-    }
-    case 'SendAgentsToTraining': {
-      return `Agent IDs: ${logIds(action)}`
-    }
-    case 'RecallAgents': {
-      return `Agent IDs: ${logIds(action)}`
-    }
-  }
+  return formatString(
+    actionToDisplayedDetailsMap[action.Action],
+    logIds(action),
+    action.TargetId,
+  )
 }
 
-function logIds(action: PlayerActionPayload): string {
-  return str(action.Ids!.sort((left, right) => left - right))
+function formatString(
+  template: string,
+  ids: string | undefined,
+  targetId: number | undefined,
+): string {
+  let formatted = template
+  if (!_.isUndefined(ids)) {
+    formatted = _.replace(formatted, '$IDs', ids)
+  }
+  if (!_.isUndefined(targetId)) {
+    formatted = _.replace(formatted, '$TargetID', targetId.toString())
+  }
+  return formatted
+}
+
+function logIds(action: PlayerActionPayload): string | undefined {
+  if (_.isUndefined(action.Ids)) {
+    return undefined
+  }
+  return str(action.Ids.sort((left, right) => left - right))
 }
