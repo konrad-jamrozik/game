979ab8b353a93f1691d20ab731528d0abf029fbb
Konrad Jamrozik <kojamroz@microsoft.com>
2024-06-06 22:58:31 -0700

    WIP: new GameSessionTurn

--- src/game-lib/State/GameSessionTurn2.cs
+++ src/game-lib/State/GameSessionTurn2.cs
@@ -6,29 +6,50 @@ namespace UfoGameLib.State;
 
 public class GameSessionTurn2
 {
-    public readonly List<GameEvent> GameEventsUntilStartGameState;
-    public readonly GameState StartGameState;
+    public readonly List<GameEvent> EventsUntilStartState;
+    public readonly GameState StartState;
     
-    public readonly List<GameEvent> GameEventsUntilGameState;
-    public readonly GameState GameState;
+    public readonly List<GameEvent> EventsInTurn;
+    public readonly GameState EndState;
 
     public GameSessionTurn2(
-        List<GameEvent>? gameEventsUntilStartGameState = null,
-        GameState? startGameState = null,
-        List<GameEvent>? gameEventsUntilGameState = null,
-        GameState? gameState = null)
+        List<GameEvent>? eventsUntilStartState = null,
+        GameState? startState = null,
+        List<GameEvent>? eventsInTurn = null,
+        GameState? endState = null)
     {
-        GameEventsUntilStartGameState = gameEventsUntilStartGameState ?? new List<GameEvent>();
-        StartGameState = startGameState ?? GameState.NewInitialGameState();
-        GameEventsUntilGameState = gameEventsUntilGameState ?? new List<GameEvent>();
-        GameState = gameState ?? StartGameState.Clone();
+        EventsUntilStartState = eventsUntilStartState ?? new List<GameEvent>();
+        StartState = startState ?? GameState.NewInitialGameState();
+        EventsInTurn = eventsInTurn ?? new List<GameEvent>();
+        EndState = endState ?? StartState.Clone();
 
         AssertInvariants();
     }
 
     private void AssertInvariants()
     {
-        //Contract.AssertImplication(GameEventsUntilStartGameState.Any(), GameEventsUntilGameState.First() is AdvanceTimePlayerAction);
+        Contract.AssertImplication(
+            EventsUntilStartState.Any(),
+            EventsUntilStartState.First().Type == nameof(AdvanceTimePlayerAction),
+            "If there are any events leading up to the start game state, the first one must be the Advance Time player action.");
+
+        Contract.AssertImplication(
+            EventsUntilStartState.Any(),
+            EventsUntilStartState.Skip(1).All(gameEvent => gameEvent is WorldEvent),
+            "If there are any events leading up to the start game state, all of them except the first one must be world events.");
+
+        Contract.Assert(
+            StartState.Timeline.CurrentTurn == EndState.Timeline.CurrentTurn,
+            "Both game states in the turn must denote the same turn.");
+
+        Contract.Assert(
+            EndState.UpdateCount >= StartState.UpdateCount,
+            "End state must have same or more updates than start state.");
+
+        Contract.Assert(
+            EventsInTurn.All(
+                gameEvent => gameEvent is PlayerActionEvent && gameEvent.Type != nameof(AdvanceTimePlayerAction)),
+            "All events in turn must be player actions and none of them can be time advancement.");
     }
 
     // public GameSessionTurn2 Clone()
