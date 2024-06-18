517120fc0da318279a55c4f98e9a917be2ff4b81
Konrad Jamrozik <kojamroz@microsoft.com>
2024-06-17 23:26:12 -0700

    remove already-addressed todo

--- src/game-lib/Model/Missions.cs
+++ src/game-lib/Model/Missions.cs
@@ -24,12 +24,6 @@ public class Missions : List<Mission>
     {
         return new Missions(this.Select(mission =>
         {
-            // kja (fixed already, but need to add assertions) getting "more than one elem" here on BasicAIPlayer plays game test.
-            // This denotes issue with busted site IDs with the new mission generation logic
-            // Root cause identified: see UfoGameLib.Model.Faction.CreateMissionSites
-            //
-            // This dup key issue was not immediately caught, and I got in UI two mission sited with ID "1".
-            // Only next turn errored with Error: "An item with the same key has already been added. Key: 1"
             MissionSite clonedMissionSite = clonedMissionSites.Single(clonedSite => clonedSite.Id == mission.Site.Id);
             return mission.DeepClone(clonedMissionSite);
         }));
