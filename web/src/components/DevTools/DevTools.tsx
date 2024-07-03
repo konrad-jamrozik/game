ccc05948be1217b9627cbf4a1eeb71ed6a45d37d
Konrad Jamrozik <kojamroz@microsoft.com>
2024-07-03 00:10:42 -0700

    improve DevTools

--- web/src/components/DevTools/DevTools.tsx
+++ web/src/components/DevTools/DevTools.tsx
@@ -40,28 +40,31 @@ function onRender(
   id: string,
   phase: 'mount' | 'update' | 'nested-update',
   actualDuration: number,
-  baseDuration: number,
+  _baseDuration: number,
   startTime: number,
   commitTime: number,
 ): void {
   console.log(
     `Profiler.onRender: id: ${_.padStart(id, 13)}, phase: ${_.padStart(phase, 13)}, ` +
-      `actualDuration: ${seconds(actualDuration)}, baseDuration: ${seconds(baseDuration)}, ` +
-      `startTime: ${seconds(startTime)}, commitTime: ${seconds(commitTime)}`,
+      `Dur: ${seconds(actualDuration)}, ` +
+      `StrT: ${seconds(startTime)}, CmtT: ${seconds(commitTime)}, ` +
+      `DelT: ${seconds(commitTime - startTime)}`,
   )
 }
 
 function seconds(milliseconds: number): string {
-  const value = (Math.round(milliseconds / 100) / 10).toString()
+  const value = (Math.round(milliseconds / 10) / 100).toString()
 
   const [integer, decimal] = _.split(value, '.')
 
   // Ensure the integer part is padded to have a length of 3 (for example), so the dot is always at the same spot
   const paddedInteger = _.padStart(integer, 3, ' ')
 
+  const paddedDecimal = _.padEnd(decimal, 2, '0')
+
   // Reconstruct the value with the padded integer part and the decimal part
   // Ensuring there's always one decimal digit
-  const paddedValue = `${paddedInteger}.${(decimal ?? '') || '0'}`
+  const paddedValue = `${paddedInteger}.${paddedDecimal}`
 
   return paddedValue
 }
