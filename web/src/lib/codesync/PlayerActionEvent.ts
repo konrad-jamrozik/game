8a166c77b2cb53b74467dab94ad6c3a07d2a53ba
Konrad Jamrozik <kojamroz@microsoft.com>
2024-07-09 22:52:37 -0700

    dedup type and const array definitions of player actions

--- web/src/lib/codesync/PlayerActionEvent.ts
+++ web/src/lib/codesync/PlayerActionEvent.ts
@@ -6,33 +6,27 @@ export type PlayerActionEvent = GameEventBase & {
   readonly TargetId: number | undefined
 }
 
-export type PlayerActionName =
-  | 'AdvanceTimePlayerAction'
-  | 'BuyTransportCapacityPlayerAction'
-  | 'HireAgentsPlayerAction'
-  | 'LaunchMissionPlayerAction'
-  | 'InvestIntelPlayerAction'
-  | AgentPlayerActionName
+export const AgentPlayerActionNameVal = [
+  'RecallAgentsPlayerAction',
+  'SackAgentsPlayerAction',
+  'SendAgentsToGatherIntelPlayerAction',
+  'SendAgentsToGenerateIncomePlayerAction',
+  'SendAgentsToTrainingPlayerAction',
+] as const
 
-export type AgentPlayerActionName =
-  | 'SackAgentsPlayerAction'
-  | 'SendAgentsToGenerateIncomePlayerAction'
-  | 'SendAgentsToGatherIntelPlayerAction'
-  | 'SendAgentsToTrainingPlayerAction'
-  | 'RecallAgentsPlayerAction'
+export type AgentPlayerActionName = (typeof AgentPlayerActionNameVal)[number]
 
-export const PlayerActionNameVal: PlayerActionName[] = [
+export const PlayerActionNameVal = [
   'AdvanceTimePlayerAction',
   'BuyTransportCapacityPlayerAction',
   'HireAgentsPlayerAction',
   'InvestIntelPlayerAction',
   'LaunchMissionPlayerAction',
   'SackAgentsPlayerAction',
-  'SendAgentsToGenerateIncomePlayerAction',
-  'SendAgentsToGatherIntelPlayerAction',
-  'SendAgentsToTrainingPlayerAction',
-  'RecallAgentsPlayerAction',
-]
+  ...AgentPlayerActionNameVal,
+] as const
+
+export type PlayerActionName = (typeof PlayerActionNameVal)[number]
 
 export type PlayerActionNameInTurn = Exclude<
   PlayerActionName,
